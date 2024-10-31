b8f4ac2b84cb56398eacb6c783c8d3c216d9dcc7
MuraliC <luckyleo1204@gmail.com>
2024-10-30 22:35:29 -0400

    Update4

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
   @Test
   public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Hello Murali Test"));
+    assertThat(greeter.sayHello(), containsString("Hello"));
   }
 
 }
